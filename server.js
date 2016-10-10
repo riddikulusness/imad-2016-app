01fb47b4f60b90dad739be1ec477a78120303c80
Edith Andrews <edith.andrews@gmail.com>
2016-10-10 16:57:30 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -23,6 +23,11 @@ var articleone = {
 };
 
 function createTemplate (data){
+    var title = data.title;
+    var date = data.date;
+    var heading = data.heading;
+    var content = data.content;
+
 var htmlTemplate `
 <html>
     <head>
@@ -52,13 +57,14 @@ var htmlTemplate `
     </div>
 </html>
 `;
+return htmlTemplate;
 }
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
 app.get('/article-one', function (req, res){
-     res.sendFile(path.join(__dirname, 'ui', 'article-one.html'));
+     res.send(createTemplate(ArticleOne));
 });
 app.get('/article-two', function (req, res){
    res.sendFile(path.join(__dirname, 'ui', 'article-two.html'));
