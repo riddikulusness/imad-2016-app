d8ee95abb56d57f02496c8a2187f81d633640685
Edith Andrews <edith.andrews@gmail.com>
2016-10-10 21:51:30 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -2,3 +2,8 @@ console.log('Loaded!');
 //change the text of the main-text div
 var element = document.getElementById('main-text');
 element.innerHTML = 'Hello';
+//move the image
+var img = document.getElementById('img');
+img.onclick =function () {
+    img.style.marginLeft = '100px';
+}
