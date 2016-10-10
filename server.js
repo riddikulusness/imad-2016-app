8e011ae92ac6b587a020a59e6b64f4d72bfb1ac9
Edith Andrews <edith.andrews@gmail.com>
2016-10-10 21:12:41 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -98,8 +98,13 @@ app.get('/:articleName', function (req, res){
     var articleName = req.params.articleName;
      res.send(createTemplate(articles[articleName]));
 });
+
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
+});
+
+app.get('/ui/style.js', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'style.js'));
 });
 
 app.get('/ui/madi.png', function (req, res) {
