080dd9557a68830c8c87f76a829fcff0dbf217bd
Edith Andrews <edith.andrews@gmail.com>
2016-10-10 22:01:09 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -7,9 +7,9 @@ var img = document.getElementById('madi');
 img.onclick =function () {
     var marginLeft = 0;
     function moveRight(){
-        marginLeft= marginLeft + 10;
+        marginLeft= marginLeft + 1;
         img.style.marginLeft = marginLeft + 'px';
     }
-    var interval = setInterval(moveRight, 100)
+    var interval = setInterval(moveRight, 50)
     img.style.marginLeft = '100px';
 }
